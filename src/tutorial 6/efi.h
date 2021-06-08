b396896cc060d182b3a10a41c586e3331ae42bf7
ThatOSDev <starlordgo@protonmail.com>
2021-06-08 18:46:47 -0400

    Added Cursor Positions and ClearScreen function

---
+++ src/tutorial 6/efi.h
@@ -0,0 +1,160 @@
+// UEFI From Scratch Tutorials - ThatOSDev ( 2021 )
+// https://github.com/ThatOSDev/UEFI-Tuts
+
+#ifndef EFI_H
+#define EFI_H
+
+#define EFI_BLACK                               0x00
+#define EFI_BLUE                                0x01
+#define EFI_GREEN                               0x02
+#define EFI_CYAN                                0x03
+#define EFI_RED                                 0x04
+#define EFI_MAGENTA                             0x05
+#define EFI_BROWN                               0x06
+#define EFI_LIGHTGRAY                           0x07
+#define EFI_DARKGRAY                            0x08
+#define EFI_LIGHTBLUE                           0x09
+#define EFI_LIGHTGREEN                          0x0A
+#define EFI_LIGHTCYAN                           0x0B
+#define EFI_LIGHTRED                            0x0C
+#define EFI_LIGHTMAGENTA                        0x0D
+#define EFI_YELLOW                              0x0E
+#define EFI_WHITE                               0x0F
+                                               
+#define EFI_BACKGROUND_BLACK                    0x00
+#define EFI_BACKGROUND_BLUE                     0x10
+#define EFI_BACKGROUND_GREEN                    0x20
+#define EFI_BACKGROUND_CYAN                     0x30
+#define EFI_BACKGROUND_RED                      0x40
+#define EFI_BACKGROUND_MAGENTA                  0x50
+#define EFI_BACKGROUND_BROWN                    0x60
+#define EFI_BACKGROUND_LIGHTGRAY                0x70
+
+// NOTE : CHAR16 is supposed to be Minimum of 16-Bit.
+//        But on some machines it will be 32-Bit.
+//        I included both typedefs as a means of reference.
+typedef unsigned short int  uint16_t;
+typedef unsigned short int  uint_least16_t;
+typedef uint_least16_t          CHAR16;
+
+typedef unsigned short      UINT16;
+typedef unsigned int        UINT32;
+typedef unsigned long long  UINT64;
+
+typedef int                 INT32;
+
+// UINTN can be used for both 64-Bit and 32-Bit.
+// We set this for 64-Bit since this tutorial series is 64-Bit only.
+typedef unsigned long long      UINTN;
+
+typedef unsigned char       BOOLEAN;
+
+typedef void                *EFI_HANDLE;
+typedef UINT64              EFI_STATUS;
+typedef void                *EFI_EVENT;
+
+// We are forward declaring these structs so that the function typedefs can operate.
+struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+
+// We check for the scan code of the key with this struct
+// Notice the 16-Bit interface, instead of the normal 8-Bit.
+// This is because EFI uses UNICODE.
+typedef struct EFI_INPUT_KEY
+{
+	UINT16    ScanCode;
+	UINT16    UnicodeChar;
+}EFI_INPUT_KEY;
+
+// We check for the event.
+typedef void(*EFI_EVENT_NOTIFY)(EFI_EVENT Event, void *Context);
+
+// We use this to reset the buffer.
+typedef EFI_STATUS (*EFI_INPUT_RESET)(struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This, BOOLEAN ExtendedVerification);
+
+// This is where we actually read the key stroke from the keyboard buffer.
+typedef EFI_STATUS (*EFI_INPUT_READ_KEY)(struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This, EFI_INPUT_KEY *Key);
+
+// The struct for the keyboard input.
+typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL
+{
+	EFI_INPUT_RESET        Reset;
+	EFI_INPUT_READ_KEY     ReadKeyStroke;
+	EFI_EVENT              WaitForKey;
+} EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+
+// We use this for setting the cursor position.
+typedef struct SIMPLE_TEXT_OUTPUT_MODE
+{
+    INT32                       MaxMode;
+    INT32                       Mode;
+    INT32                       Attribute;
+    INT32                       CursorColumn;
+    INT32                       CursorRow;
+    BOOLEAN                     CursorVisible;
+} SIMPLE_TEXT_OUTPUT_MODE;
+
+// This function resets the string output.
+typedef EFI_STATUS (*EFI_TEXT_RESET)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, BOOLEAN ExtendedVerification);
+
+// This function prints the string output to the screen.
+typedef EFI_STATUS (*EFI_TEXT_STRING)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
+
+// We are not using these next three functions,
+// but they are required in order for the struct to be filled out.
+typedef EFI_STATUS (*EFI_TEXT_TEST_STRING)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
+typedef EFI_STATUS (*EFI_TEXT_QUERY_MODE)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, UINTN ModeNumber, UINTN *Columns, UINTN *Rows);
+typedef EFI_STATUS (*EFI_TEXT_SET_MODE)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, UINTN ModeNumber);
+
+// This is the function we use to set the color attribute of the text.
+typedef EFI_STATUS (*EFI_TEXT_SET_ATTRIBUTE)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, UINTN Attribute);
+
+// This function we use to clear the screen buffer.
+typedef EFI_STATUS (*EFI_TEXT_CLEAR_SCREEN)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This);
+
+// This function is used to set the cursor position.
+typedef EFI_STATUS (*EFI_TEXT_SET_CURSOR_POSITION)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, UINTN Column, UINTN Row);
+
+// With this we can make the cursor visible or not.
+typedef EFI_STATUS (*EFI_TEXT_ENABLE_CURSOR)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, BOOLEAN Visible);
+
+// The struct for the EFI Text Output protocols.
+typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
+{
+    EFI_TEXT_RESET                         Reset;
+    EFI_TEXT_STRING                        OutputString;
+    EFI_TEXT_TEST_STRING                   TestString;
+    EFI_TEXT_QUERY_MODE                    QueryMode;
+    EFI_TEXT_SET_MODE                      SetMode;
+    EFI_TEXT_SET_ATTRIBUTE                 SetAttribute;
+    EFI_TEXT_CLEAR_SCREEN                  ClearScreen;
+    EFI_TEXT_SET_CURSOR_POSITION           SetCursorPosition;
+    EFI_TEXT_ENABLE_CURSOR                 EnableCursor;
+    SIMPLE_TEXT_OUTPUT_MODE                *Mode;
+} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+
+// This is the main EFI header for all of the EFI.
+typedef struct EFI_TABLE_HEADER
+{
+    UINT64    Signature;
+    UINT32    Revision;
+    UINT32    HeaderSize;
+    UINT32    CRC32;
+    UINT32    Reserved;
+} EFI_TABLE_HEADER;
+
+// EFI has a system and runtime. This system table is the first struct
+// called from the main section. Think of it as the entry point
+// to all of the EFI functions.
+typedef struct EFI_SYSTEM_TABLE
+{
+    EFI_TABLE_HEADER                hrd;
+    CHAR16                          *FirmwareVendor;
+    UINT32                          FirmwareVersion;
+    EFI_HANDLE                      ConsoleInHandle;
+    EFI_SIMPLE_TEXT_INPUT_PROTOCOL  *ConIn;
+    EFI_HANDLE                      ConsoleOutHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
+} EFI_SYSTEM_TABLE ;
+
+#endif // EFI_H
---
+++ src/tutorial 6/efimain.c
@@ -0,0 +1,46 @@
+// UEFI From Scratch Tutorials - ThatOSDev ( 2021 )
+// https://github.com/ThatOSDev/UEFI-Tuts
+
+#include "efi.h"
+#include "ErrorCodes.h"
+#include "libs.h"
+
+// This is like int main() in a typical C program.
+// In this case, we create an ImageHandle for the overall EFI interface,
+// as well as a System Table pointer to the EFI_SYSTEM_TABLE struct.
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *ST)
+{
+    // We setup this global variable in the libs.h file.
+    SystemTable = ST;
+    
+    ResetScreen();
+    
+    SetColor(EFI_WHITE);
+    SetTextPosition(3, 2);
+    Print(L"Welcome to the UEFI From Scratch Tutorials\r\n\r\n");
+    
+    SetColor(EFI_YELLOW);
+    SetTextPosition(3, 5);
+    Print(L"We have Simple-Text Cursor Positions.");
+    
+    SetColor(EFI_GREEN);
+    SetTextPosition(8, 8);
+    Print(L"Hit Any Key to Clear the Screen");
+    
+    HitAnyKey();
+    
+    ClearScreen();
+
+    SetColor(EFI_CYAN);
+    SetTextPosition(2, 1);
+    Print(L"The Screen has been cleared !");
+    
+    SetColor(EFI_GREEN);
+    SetTextPosition(2, 4);
+    Print(L"Hit Any Key");
+
+    HitAnyKey();
+
+    // The EFI needs to have a 0 ( or EFI_SUCCESS ) in order to know everything is ok.
+    return 0;
+}
