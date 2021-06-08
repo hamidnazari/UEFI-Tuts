76efbbfe4cb67e2ab478368e34f73f1c239ae5d4
ThatOSDev <starlordgo@protonmail.com>
2021-06-08 17:49:53 -0400

    Fixed ErrorCode return Type

--- src/tutorial 4/ErrorCodes.h
+++ src/tutorial 4/ErrorCodes.h
@@ -1,6 +1,8 @@
+// UEFI Bootable Space Invaders Game - ThatOSDev ( 2021 )
+// https://github.com/ThatOSDev/UEFI-Space-Invaders
+
 #pragma once
 
-// Page 2212 - UEFI Specs 2.8b
 #define EFI_SUCCESS               0x0000000000000000
 #define EFI_ERROR                 0x8000000000000000 
 #define EFI_LOAD_ERROR            (EFI_ERROR | 0x0000000000000001)
@@ -37,7 +39,6 @@
 #define EFI_IP_ADDRESS_CONFLICT   (EFI_ERROR | 0x0000000000000020)
 #define EFI_HTTP_ERROR            (EFI_ERROR | 0x0000000000000021)
 
-// Page 2214 - UEFI Specs 2.8b
 #define EFI_WARN_UNKNOWN_GLYPH     0x0000000000000001
 #define EFI_WARN_DELETE_FAILURE    0x0000000000000002
 #define EFI_WARN_WRITE_FAILURE     0x0000000000000003
@@ -47,174 +48,174 @@
 #define EFI_WARN_RESET_REQUIRED    0x0000000000000007
 
 // Adding this for convenience
-unsigned short* CheckStandardEFIError(unsigned long long s)
+unsigned short int* CheckStandardEFIError(unsigned long long s)
 {
     switch(s)
     {
         case EFI_LOAD_ERROR:
         {
-            return (unsigned short*)L" Load Error\r\n";
+            return (unsigned short int*)L" Load Error\r\n";
         }
         case EFI_INVALID_PARAMETER:
         {
-            return (unsigned short*)L" Invalid Parameter\r\n";
+            return (unsigned short int*)L" Invalid Parameter\r\n";
         }
         case EFI_UNSUPPORTED:
         {
-            return (unsigned short*)L" Unsupported\r\n";
+            return (unsigned short int*)L" Unsupported\r\n";
         }
         case EFI_BAD_BUFFER_SIZE:
         {
-            return (unsigned short*)L" Bad Buffer Size\r\n";
+            return (unsigned short int*)L" Bad Buffer Size\r\n";
         }
         case EFI_BUFFER_TOO_SMALL:
         {
-            return (unsigned short*)L" Buffer Too Small\r\n";
+            return (unsigned short int*)L" Buffer Too Small\r\n";
         }
         case EFI_NOT_READY:
         {
-            return (unsigned short*)L" Not Ready\r\n";
+            return (unsigned short int*)L" Not Ready\r\n";
         }
         case EFI_DEVICE_ERROR:
         {
-            return (unsigned short*)L" Device Error\r\n";
+            return (unsigned short int*)L" Device Error\r\n";
         }
         case EFI_WRITE_PROTECTED:
         {
-            return (unsigned short*)L" Write Protected\r\n";
+            return (unsigned short int*)L" Write Protected\r\n";
         }
         case EFI_OUT_OF_RESOURCES:
         {
-            return (unsigned short*)L" Out Of Resources\r\n";
+            return (unsigned short int*)L" Out Of Resources\r\n";
         }
         case EFI_VOLUME_CORRUPTED:
         {
-            return (unsigned short*)L" Volume Corrupted\r\n";
+            return (unsigned short int*)L" Volume Corrupted\r\n";
         }
         case EFI_VOLUME_FULL:
         {
-            return (unsigned short*)L" Volume Full\r\n";
+            return (unsigned short int*)L" Volume Full\r\n";
         }
         case EFI_NO_MEDIA:
         {
-            return (unsigned short*)L" No Media\r\n";
+            return (unsigned short int*)L" No Media\r\n";
         }
         case EFI_MEDIA_CHANGED:
         {
-            return (unsigned short*)L" Media Changed\r\n";
+            return (unsigned short int*)L" Media Changed\r\n";
         }
         case EFI_NOT_FOUND:
         {
-            return (unsigned short*)L" File Not Found\r\n";
+            return (unsigned short int*)L" File Not Found\r\n";
         }
         case EFI_ACCESS_DENIED:
         {
-            return (unsigned short*)L" Access Denied\r\n";
+            return (unsigned short int*)L" Access Denied\r\n";
         }
         case EFI_NO_RESPONSE:
         {
-            return (unsigned short*)L" No Response\r\n";
+            return (unsigned short int*)L" No Response\r\n";
         }
         case EFI_NO_MAPPING:
         {
-            return (unsigned short*)L" No Mapping\r\n";
+            return (unsigned short int*)L" No Mapping\r\n";
         }
         case EFI_TIMEOUT:
         {
-            return (unsigned short*)L" Timeout\r\n";
+            return (unsigned short int*)L" Timeout\r\n";
         }
         case EFI_NOT_STARTED:
         {
-            return (unsigned short*)L" Not Started\r\n";
+            return (unsigned short int*)L" Not Started\r\n";
         }
         case EFI_ALREADY_STARTED:
         {
-            return (unsigned short*)L" Already Started\r\n";
+            return (unsigned short int*)L" Already Started\r\n";
         }
         case EFI_ABORTED:
         {
-            return (unsigned short*)L" Aborted\r\n";
+            return (unsigned short int*)L" Aborted\r\n";
         }
         case EFI_ICMP_ERROR:
         {
-            return (unsigned short*)L" ICMP Error\r\n";
+            return (unsigned short int*)L" ICMP Error\r\n";
         }
         case EFI_TFTP_ERROR:
         {
-            return (unsigned short*)L" TFTP Error\r\n";
+            return (unsigned short int*)L" TFTP Error\r\n";
         }
         case EFI_PROTOCOL_ERROR:
         {
-            return (unsigned short*)L" Protocol Error\r\n";
+            return (unsigned short int*)L" Protocol Error\r\n";
         }
         case EFI_INCOMPATIBLE_VERSION:
         {
-            return (unsigned short*)L" Incompatible Version\r\n";
+            return (unsigned short int*)L" Incompatible Version\r\n";
         }
         case EFI_SECURITY_VIOLATION:
         {
-            return (unsigned short*)L" Security Violation\r\n";
+            return (unsigned short int*)L" Security Violation\r\n";
         }
         case EFI_CRC_ERROR:
         {
-            return (unsigned short*)L" CRC Error\r\n";
+            return (unsigned short int*)L" CRC Error\r\n";
         }
         case EFI_END_OF_MEDIA:
         {
-            return (unsigned short*)L" End Of Media\r\n";
+            return (unsigned short int*)L" End Of Media\r\n";
         }
         case EFI_END_OF_FILE:
         {
-            return (unsigned short*)L" End Of File\r\n";
+            return (unsigned short int*)L" End Of File\r\n";
         }
         case EFI_INVALID_LANGUAGE:
         {
-            return (unsigned short*)L" Invalid Language\r\n";
+            return (unsigned short int*)L" Invalid Language\r\n";
         }
         case EFI_COMPROMISED_DATA:
         {
-            return (unsigned short*)L" Compromised Data\r\n";
+            return (unsigned short int*)L" Compromised Data\r\n";
         }
         case EFI_IP_ADDRESS_CONFLICT:
         {
-            return (unsigned short*)L" IP Address Conflict\r\n";
+            return (unsigned short int*)L" IP Address Conflict\r\n";
         }
         case EFI_HTTP_ERROR:
         {
-            return (unsigned short*)L" End Of File\r\n";
+            return (unsigned short int*)L" End Of File\r\n";
         }
         case EFI_WARN_UNKNOWN_GLYPH:
         {
-            return (unsigned short*)L" WARNING - Unknown Glyph\r\n";
+            return (unsigned short int*)L" WARNING - Unknown Glyph\r\n";
         }
         case EFI_WARN_DELETE_FAILURE:
         {
-            return (unsigned short*)L" WARNING - Delete Failure\r\n";
+            return (unsigned short int*)L" WARNING - Delete Failure\r\n";
         }
         case EFI_WARN_WRITE_FAILURE:
         {
-            return (unsigned short*)L" WARNING - Write Failure\r\n";
+            return (unsigned short int*)L" WARNING - Write Failure\r\n";
         }
         case EFI_WARN_BUFFER_TOO_SMALL:
         {
-            return (unsigned short*)L" WARNING - Buffer Too Small\r\n";
+            return (unsigned short int*)L" WARNING - Buffer Too Small\r\n";
         }
         case EFI_WARN_STALE_DATA:
         {
-            return (unsigned short*)L" WARNING - Stale Data\r\n";
+            return (unsigned short int*)L" WARNING - Stale Data\r\n";
         }
         case EFI_WARN_FILE_SYSTEM:
         {
-            return (unsigned short*)L" WARNING - File System\r\n";
+            return (unsigned short int*)L" WARNING - File System\r\n";
         }
         case EFI_WARN_RESET_REQUIRED:
         {
-            return (unsigned short*)L" WARNING - Reset Required\r\n";
+            return (unsigned short int*)L" WARNING - Reset Required\r\n";
         }
         case EFI_SUCCESS:
         {
-            return (unsigned short*)L" Successful\r\n";
+            return (unsigned short int*)L" Successful\r\n";
         }
     }
-    return (unsigned short*)L" ERROR\r\n";
+    return (unsigned short int*)L" ERROR\r\n";
 }
