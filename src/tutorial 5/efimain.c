a439eece61e0718497da990635b122e2d9da0d06
ThatOSDev <starlordgo@protonmail.com>
2021-06-08 18:27:18 -0400

    Added a libs.h file

---
+++ src/tutorial 5/efimain.c
@@ -0,0 +1,34 @@
+// UEFI From Scratch Tutorials - ThatOSDev ( 2021 )
+// https://github.com/ThatOSDev/UEFI-Tuts
+
+#include "efi.h"
+#include "ErrorCodes.h"
+#include "libs.h"
+
+// This is like int main() in a typical C program.
+// In this case, we create an ImageHandle for the overall EFI interface,
+// as well as a System Table pointer to the EFI_SYSTEM_TABLE struct.
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *ST)
+{
+    // We setup this global variable in the libs.h file.
+    SystemTable = ST;
+    
+    ResetScreen();
+    
+    SetColor(EFI_WHITE);
+    
+    Print(L"Welcome to the UEFI From Scratch Tutorials\r\n\r\n");
+    
+    SetColor(EFI_YELLOW);
+
+    Print(L"We have a libs file.\r\n\r\n");
+    
+    SetColor(EFI_GREEN);
+    
+    Print(L"\r\nHit Any Key\r\n");
+    
+    HitAnyKey();
+
+    // The EFI needs to have a 0 ( or EFI_SUCCESS ) in order to know everything is ok.
+    return 0;
+}
---
+++ src/tutorial 5/libs.h
@@ -0,0 +1,39 @@
+// UEFI From Scratch Tutorials - ThatOSDev ( 2021 )
+// https://github.com/ThatOSDev/UEFI-Tuts
+
+#ifndef LIBS_H
+#define LIBS_H
+
+EFI_SYSTEM_TABLE* SystemTable;
+
+void ResetScreen()
+{
+    // This resets the whole console ( A.K.A. your display screen ) interface.
+    SystemTable->ConOut->Reset(SystemTable->ConOut, 1);
+}
+
+void SetColor(UINTN Attribute)
+{
+    // We set the color for the text output.
+    SystemTable->ConOut->SetAttribute(SystemTable->ConOut, Attribute);
+}
+
+void Print(CHAR16* str)
+{
+    // Text Output  ( A.K.A. ConOut is Console Out )
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, str);
+}
+
+void HitAnyKey()
+{
+    // This clears the keyboard buffer.
+    SystemTable->ConIn->Reset(SystemTable->ConIn, 1);
+
+    // We setup the struct to take keyboard input.
+    EFI_INPUT_KEY Key;
+
+    // In a while loop to see if the keyboard has a key stroke in the buffer.
+    while((SystemTable->ConIn->ReadKeyStroke(SystemTable->ConIn, &Key)) == EFI_NOT_READY);
+}
+
+#endif // LIBS_H
