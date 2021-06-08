67d9f4e5622fda3039e21e17ded569faabd75ad8
ThatOSDev <starlordgo@protonmail.com>
2021-06-08 15:27:40 -0400

    Added Text Color tut

---
+++ src/tutorial 2/efimain.c
@@ -0,0 +1,124 @@
+// UEFI From Scratch Tutorials - ThatOSDev ( 2021 )
+// https://github.com/ThatOSDev/UEFI-Tuts
+
+#define EFI_BLACK                               0x00
+#define EFI_BLUE                                0x01
+#define EFI_GREEN                               0x02
+#define EFI_CYAN                                0x03
+#define EFI_RED                                 0x04
+#define EFI_MAGENTA                             0x05
+#define EFI_BROWN                               0x06
+#define EFI_LIGHTGRAY                           0x07
+#define EFI_DARKGRAY                            0x08
+#define EFI_LIGHTBLUE                           0x09
+#define EFI_LIGHTGREEN                          0x0A
+#define EFI_LIGHTCYAN                           0x0B
+#define EFI_LIGHTRED                            0x0C
+#define EFI_LIGHTMAGENTA                        0x0D
+#define EFI_YELLOW                              0x0E
+#define EFI_WHITE                               0x0F
+                                               
+#define EFI_BACKGROUND_BLACK                    0x00
+#define EFI_BACKGROUND_BLUE                     0x10
+#define EFI_BACKGROUND_GREEN                    0x20
+#define EFI_BACKGROUND_CYAN                     0x30
+#define EFI_BACKGROUND_RED                      0x40
+#define EFI_BACKGROUND_MAGENTA                  0x50
+#define EFI_BACKGROUND_BROWN                    0x60
+#define EFI_BACKGROUND_LIGHTGRAY                0x70
+
+// NOTE : CHAR16 is supposed to be Minimum of 16-Bit.
+//        But on some machines it will be 32-Bit.
+//        I included both typedefs as a means of reference.
+typedef unsigned short int  uint16_t;
+typedef unsigned short int  uint_least16_t;
+typedef uint_least16_t          CHAR16;
+
+typedef unsigned int        UINT32;
+typedef unsigned long long  UINT64;
+
+// UINTN is used for both 64-Bit and 32-Bit.
+// We set this for 64-Bit since this tutorial series is 64-Bit only.
+typedef unsigned long long      UINTN;
+
+typedef unsigned char       BOOL;
+
+typedef void                *EFI_HANDLE;
+typedef UINT64              EFI_STATUS;
+
+// This struct is a placeholder and not usable at this time
+// The code will not compile without it though.
+typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL {} EFI_SIMPLE_TEXT_INPUT_PROTOCOL;
+
+// We are forward declaring this struct so that the two function typedefs can operate.
+struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+
+// This function resets the string output.
+typedef EFI_STATUS (*EFI_TEXT_RESET)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, BOOL ExtendedVerification);
+
+// This function prints the string output to the screen.
+typedef EFI_STATUS (*EFI_TEXT_STRING)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
+
+// We are not using these next three functions,
+// but they are required in order for the struct to be filled out.
+typedef EFI_STATUS (*EFI_TEXT_TEST_STRING)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, CHAR16 *String);
+typedef EFI_STATUS (*EFI_TEXT_QUERY_MODE)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, UINTN ModeNumber, UINTN *Columns, UINTN *Rows);
+typedef EFI_STATUS (*EFI_TEXT_SET_MODE)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, UINTN ModeNumber);
+
+// This is the function we use to set the collor attribute of the text.
+typedef EFI_STATUS (*EFI_TEXT_SET_ATTRIBUTE)(struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, UINTN Attribute);
+
+// The struct for the EFI Text Output protocols.
+typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL
+{
+    EFI_TEXT_RESET                         Reset;
+    EFI_TEXT_STRING                        OutputString;
+    EFI_TEXT_TEST_STRING                   TestString;
+    EFI_TEXT_QUERY_MODE                    QueryMode;
+    EFI_TEXT_SET_MODE                      SetMode;
+    EFI_TEXT_SET_ATTRIBUTE                 SetAttribute;
+} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+
+// This is the main EFI header for all of the EFI.
+typedef struct EFI_TABLE_HEADER
+{
+    UINT64    Signature;
+    UINT32    Revision;
+    UINT32    HeaderSize;
+    UINT32    CRC32;
+    UINT32    Reserved;
+} EFI_TABLE_HEADER;
+
+// EFI has a system and runtime. This system table is the first struct
+// called from the main section.
+typedef struct EFI_SYSTEM_TABLE
+{
+    EFI_TABLE_HEADER                hrd;
+    CHAR16                          *FirmwareVendor;
+    UINT32                          FirmwareVersion;
+    EFI_HANDLE                      ConsoleInHandle;
+    EFI_SIMPLE_TEXT_INPUT_PROTOCOL  *ConIn;
+    EFI_HANDLE                      ConsoleOutHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
+} EFI_SYSTEM_TABLE ;
+
+// This is like int main() in a typical C program.
+// In this case, we create an ImageHandle for the overall EFI interface,
+// as well as a System Table pointer to the EFI_SYSTEM_TABLE struct.
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+    // This clears the screen and buffer.
+    SystemTable->ConOut->Reset(SystemTable->ConOut, 1);
+
+    // Here is where we set the color Yellow for the text output.
+    SystemTable->ConOut->SetAttribute(SystemTable->ConOut, EFI_YELLOW);
+
+    // This prints "Testing..." to the screen ( A.K.A. ConOut is Console Out )
+    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Testing...\r\n");
+
+    // We use this while loop to hang. At this point, simple shut off computer.
+    while(1){};
+
+    // The EFI needs to have a 0 ( or EFI_SUCCESS ) in order to know everything is ok.
+    return 0;
+}
