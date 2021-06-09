902d24ed0450ecafb4b0159f583c67560e1975d3
ThatOSDev <starlordgo@protonmail.com>
2021-06-09 00:38:15 -0400

    Added Graphics Tutorial Code

---
+++ src/tutorial 7/efimain.c
@@ -0,0 +1,54 @@
+// UEFI From Scratch Tutorials - ThatOSDev ( 2021 )
+// https://github.com/ThatOSDev/UEFI-Tuts
+
+#include "efi.h"
+#include "ErrorCodes.h"
+#include "libs.h"
+
+// This is like int main() in a typical C program.
+// In this case, we create an ImageHandle for the overall EFI interface,
+// as well as a System Table pointer to the EFI_SYSTEM_TABLE struct.
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *ST)
+{
+    // We setup this global variable in the libs.h file.
+    SystemTable = ST;
+    
+    ResetScreen();
+    
+    SetColor(EFI_WHITE);
+    SetTextPosition(3, 2);
+    Print(L"Welcome to the UEFI From Scratch Tutorials\r\n\r\n");
+    
+    SetColor(EFI_GREEN);
+    SetTextPosition(8, 4);
+    Print(L"Hit Any Key to see Graphics");
+
+    HitAnyKey();
+    
+    Print(L"\r\n\r\nLoading Graphics Output Protocol ... ");
+    EFI_STATUS Status = SystemTable->BootServices->LocateProtocol(&EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID, 0, (void**)&gop);
+    if(Status == EFI_SUCCESS)
+    {
+        SetColor(EFI_CYAN);
+        Print(CheckStandardEFIError(Status));
+        SetGraphicsColor(ORANGE);
+        CreateFilledBox(50, 50, 100, 200);
+        SetGraphicsColor(RED);
+        CreateFilledBox(60, 60, 80, 30);
+        SetColor(EFI_YELLOW);
+        SetTextPosition(5, 8);
+        Print(L"We have Graphics !!");
+    } else {
+        SetColor(EFI_RED);
+        Print(CheckStandardEFIError(Status));
+    }
+
+    SetColor(EFI_GREEN);
+    SetTextPosition(2, 10);
+    Print(L"Hit Any Key");
+
+    HitAnyKey();
+
+    // The EFI needs to have a 0 ( or EFI_SUCCESS ) in order to know everything is ok.
+    return EFI_SUCCESS;
+}
